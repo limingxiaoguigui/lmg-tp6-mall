33f27782102c18d3db2477df609e1613296bbb81
liminggui <htyzhliminggui@163.com>
2020-09-06 14:55:48 +0800

    编辑器图片上传

--- app/admin/controller/Image.php
+++ app/admin/controller/Image.php
@@ -17,6 +17,7 @@ class Image extends AdminBase
     
     /**
      * 上传图片
+     *
      * @return mixed
      * @user LMG
      * @date 2020/9/5
@@ -30,14 +31,44 @@ class Image extends AdminBase
         $file = $this -> request -> file('file');
         //todo 上传类型需要判断 文件大小限制 长宽限制
         $filename = Filesystem ::disk('public')
-                               -> putFile('upload/image', $file);
+                               -> putFile('image', $file);
         if ( ! $filename) {
             return show(config('status.error'), '上传图片失败');
         }
         //图片地址
-        $imageUrl[ 'image' ] = '/upload/'.$filename;
+        $imageUrl[ 'image' ] = '/storage/'.$filename;
         
         return show(config('status.success'), '图片上传成功！', $imageUrl);
     }
+    
+    /**
+     * 编辑器上传图片
+     *
+     * @return mixed|string
+     * @user LMG
+     * @date 2020/9/6
+     */
+    public function layUpload()
+    {
+        
+        if ( ! $this -> request -> isPost()) {
+            return show(config('status.error'), '请求不成功！');
+        }
+        $file = $this -> request -> file('file');
+        $filename = Filesystem ::disk('public')
+                               -> putFile('image', $file);
+        if ( ! $filename) {
+            return json(['code' => 1, 'data' => []], 200);
+        }
+        $result = [
+            'code' => 0,
+            'data' => [
+                'src' => '/storage/'.$filename
+            ]
+        ];
+        
+        return json($result, 200);
+    }
+    
     
 }
